c9d05479d37de854e3882a79c68149e6c035f3c9
vuedev2113 <eduarmora251@gmail.com>
2024-10-09 23:24:02 +0900

    creation of portfolio

---
+++ next.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+ 
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.dev.to',
+        pathname: '**',
+      },
+    ],
+  },
+}
